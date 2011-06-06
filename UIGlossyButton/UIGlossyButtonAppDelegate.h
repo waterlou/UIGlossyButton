e0b76f057c0679cd4f00b0f47dc07a93a47be174
Water Lou <water@waterworld.com.hk>
2011-06-06 17:49:24 +0800

    Initial Commit

---
+++ UIGlossyButton/UIGlossyButtonAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  UIGlossyButtonAppDelegate.h
+//  UIGlossyButton
+//
+//  Created by Water Lou on 06/06/2011.
+//  Copyright 2011 First Water Tech Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class UIGlossyButtonViewController;
+
+@interface UIGlossyButtonAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UIGlossyButtonViewController *viewController;
+
+@end
